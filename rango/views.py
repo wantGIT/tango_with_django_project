1a3ddbeba70b8cff5ebddeb10346ebfb75f4bdd8
James Yao <2236131y@student.gla.ac.uk>
2018-02-02 13:28:45 +0000

    finished ch4

--- rango/views.py
+++ rango/views.py
@@ -15,4 +15,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href= '/rango/'> index <a/>")
+    context_abt = {'author':'James Yao'}
+    return render(request, 'rango/about.html', context = context_abt)
