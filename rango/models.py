f4d34be38a3dbf9fe0922cd03d9829b8c08cc83f
James Yao <2236131y@student.gla.ac.uk>
2018-02-02 16:49:35 +0000

    done ch5

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -14,7 +16,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
     
     def __str__(self): # For Python 2, use __unicode__ too
         return self.title
