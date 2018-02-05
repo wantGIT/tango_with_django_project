4892c7268ed49e4f18bfb35f70bc103fd8130810
James Yao <2236131y@student.gla.ac.uk>
2018-02-05 16:41:27 +0000

    finished ch6

--- rango/urls.py
+++ rango/urls.py
@@ -4,4 +4,8 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
+    url(r'^pages/(?P<page_name_slug>[\w\-]+)/$',
+        views.show_page, name='show_page'),
 ]
